ba8e9c2f43264f68e597fac3ac8471ba5dff9731
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-04-01 16:39:16 +0530

    Utils Code

--- src/index.js
+++ src/index.js
@@ -9,3 +9,13 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port : ${
+            process.env.PORT
+        }`);
+    })
+})
+.catch((err) => {
+    console.log("MONOGO DB CONNECTION ERROR: ", err);
+})
